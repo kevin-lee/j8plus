989d13291572524c1472f4483129493e10cc2937
Kevin Lee <kevin.code@lckymn.com>
2015-05-16 18:38:26 +1000

    added: tests for Runner

---
+++ src/test/java/cc/kevinlee/functional/types/RunnerTest.java
@@ -0,0 +1,47 @@
+package cc.kevinlee.functional.types;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static cc.kevinlee.testosterone.Testosterone.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+/**
+ * @author Lee, Seong Hyun (Kevin)
+ * @since 2015-05-13
+ */
+public class RunnerTest {
+
+  @Test
+  public void testAndThen() throws Exception {
+
+    final List<Integer> resultCollector = new ArrayList<>();
+    final Runner first = () -> resultCollector.add(1);
+    final Runner second = () -> resultCollector.add(2);
+
+    test("Runner.andThen()", "runner1.andThen(runner2) should run runner1.run() then runner2.run()")
+        .when(() -> first.andThen(second).run())
+        .then(() ->
+            assertThat(resultCollector).isEqualTo(Arrays.asList(1, 2))
+        );
+
+  }
+  @Test
+  public void testAndThen2() throws Exception {
+
+    final List<Integer> resultCollector = new ArrayList<>();
+    final Runner first = () -> resultCollector.add(1);
+    final Runner second = () -> resultCollector.add(2);
+
+    test("Runner.andThen()", "runner1.andThen(runner2) should run runner1.run() then runner2.run()")
+        .when(() -> second.andThen(first).run())
+        .then(() ->
+            assertThat(resultCollector).isEqualTo(Arrays.asList(2, 1))
+        );
+
+  }
+}
