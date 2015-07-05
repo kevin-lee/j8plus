6ec4cc0e82adcb676b9315010f075dfc58f741d5
Kevin Lee <kevin.code@lckymn.com>
2015-07-05 20:01:54 +1000

    #10 - sbt should be used to build and deploy J8+ - cache control

--- .store-cache.sh
+++ .store-cache.sh
@@ -11,7 +11,7 @@ if [ ! -d "$SEMAPHORE_CACHE_DIR/app-ivy2-cache" ];
   echo "mkdir -p $SEMAPHORE_CACHE_DIR/app-ivy2-cache"
   mkdir -p "$SEMAPHORE_CACHE_DIR/app-ivy2-cache"
 fi
-cp -Rp "$HOME/.ivy2/cache" "$SEMAPHORE_CACHE_DIR/app-ivy2-cache"
+cp -Rp "$HOME/.ivy2/cache" "$SEMAPHORE_CACHE_DIR/app-ivy2-cache/"
 echo "Copy Ivy cache to $SEMAPHORE_CACHE_DIR/app-ivy2-cache: Done"
 echo "============================================"
 echo "Copy Sbt cache to $SEMAPHORE_CACHE_DIR/app-sbt-cache"
@@ -21,6 +21,6 @@ if [ ! -d "$SEMAPHORE_CACHE_DIR/app-sbt-cache" ];
   echo "mkdir -p $SEMAPHORE_CACHE_DIR/app-sbt-cache "
   mkdir -p "$SEMAPHORE_CACHE_DIR/app-sbt-cache"
 fi
-cp -Rp "$HOME/.sbt/boot" "$SEMAPHORE_CACHE_DIR/app-sbt-cache"
+cp -Rp "$HOME/.sbt/boot" "$SEMAPHORE_CACHE_DIR/app-sbt-cache/"
 echo "Copy Sbt cache to $SEMAPHORE_CACHE_DIR/app-sbt-cache: Done"
 echo "============================================"
